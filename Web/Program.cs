db5974bc723ea6a91d90edc3744652d07b0583af
Vladislav Zainullin <mittoface02@mail.ru>
2024-08-18 15:36:31 +0300

    Добавляет настройку логирования

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-var app = builder.Build();
+namespace Web;
 
-app.Run();
+file static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .WriteTo.File("Logs/log-.txt",
+                rollingInterval: RollingInterval.Hour,
+                rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+        try
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+
+            await using var app = builder.Build();
+
+            app.UseSerilogRequestLogging();
+
+            await app.RunAsync();
+        }
+        catch (HostAbortedException)
+        {
+        }
+        catch (Exception e)
+        {
+            Log.Fatal($"Application not started. {e}");
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
+        }
+    }
+}

c1dec4812ab3a36ed4a801fcf41cc8329f02eec4
VladislavZainullin <mittoface02@mail.ru>
2024-08-30 16:58:57 +0300

    Добавляет регистрацию healthcheck

--- Web/DependencyInjection.cs
+++ Web/DependencyInjection.cs
@@ -4,6 +4,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddWebServices(this IServiceCollection services)
     {
+        services.AddHealthChecks();
+        
         return services;
     }
 }

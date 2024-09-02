d306d60b7a1256bf5700b87e4d8f2e03afa24783
Vladislav Zainullin <mittoface02@mail.ru>
2024-09-02 19:57:18 +0300

    Удаляет конвенцию cascade удаления по умолчанию

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -2,12 +2,13 @@ using System.Reflection;
 using Domain.Entities.Products.Countries;
 using Domain.Entities.Products.Manufacturers;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 // ReSharper disable ReturnTypeCanBeEnumerable.Global
 
 namespace Persistence;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+internal sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Country> Countries => Set<Country>();
 
@@ -18,5 +19,11 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         
         base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Conventions.Remove(typeof(CascadeDeleteConvention));
+        base.ConfigureConventions(configurationBuilder);
     }
 }

b3824d8a83865ee43a1237d8ec95c79cb04d1b56
Vladislav Zainullin <mittoface02@mail.ru>
2024-09-06 20:44:53 +0300

    Рефакторинг

--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using Domain.Entities.Products.Countries;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 // ReSharper disable ReturnTypeCanBeEnumerable.Global
 
@@ -16,11 +15,5 @@ internal sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbC
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         
         base.OnModelCreating(modelBuilder);
-    }
-
-    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
-    {
-        configurationBuilder.Conventions.Remove(typeof(CascadeDeleteConvention));
-        base.ConfigureConventions(configurationBuilder);
     }
 }

7819cd267bc681c3341886e3e98d887443b5016c
Vladislav Zainullin <mittoface02@mail.ru>
2024-09-15 23:08:07 +0300

    Удаляет неиспользуемый контроллер

--- Web/Controllers/MeasurementUnitPositionController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Web.Controllers;
-
-[Route("api/measurement-unit-positions")]
-public sealed class MeasurementUnitPositionController : AppController
-{
-    
-}
